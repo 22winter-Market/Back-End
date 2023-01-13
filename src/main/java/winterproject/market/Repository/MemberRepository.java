4fd99669e3ba16b34b053b2a3d0228c6d46b88f4
yg0585 <yg0585@naver.com>
2023-01-13 17:51:14 +0900

    feat: id를 받아 list로 찾는 것과 하나만 찾는 것 구현

--- src/main/java/winterproject/market/Repository/MemberRepository.java
+++ src/main/java/winterproject/market/Repository/MemberRepository.java
@@ -5,18 +5,26 @@ import org.springframework.stereotype.Repository;
 import winterproject.market.domain.Member;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
 
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
     }
 
-    public Member findById(String id) {
+    public List<Member> findById(String id) {
+        return em.createQuery("select m from Member m where m.id = :id",
+                Member.class)
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    public Member findOneById(String id) {
         return em.find(Member.class, id);
     }
 }
