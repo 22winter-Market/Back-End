d43eda73ba3483abdb4976b6907685811281001c
yg0585 <yg0585@naver.com>
2023-01-17 22:23:44 +0900

    feat: 회원 가입 필드 유효성 검사

--- src/main/java/winterproject/market/controller/MemberForm.java
+++ src/main/java/winterproject/market/controller/MemberForm.java
@@ -2,15 +2,20 @@ package winterproject.market.controller;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class MemberForm {
-    @NotEmpty(message = "id는 필수입니다.")
+    @NotEmpty(message = "아이디가 입력되지 않았습니다.")
     private String id;
+
+    @NotEmpty(message = "비밀번호가 입력되지 않았습니다.")
     private String password;
+
+    @NotEmpty(message = "이메일이 입력되지 않았습니다.")
     private String email;
+
+    @NotEmpty(message = "닉네임이 입력되지 않았습니다.")
     private String nickname;
 }
