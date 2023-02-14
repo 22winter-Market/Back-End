3ad25bf299d0e0fcbefc5ea6c1f6b58f8ba63332
yg0585 <yg0585@naver.com>
2023-02-14 16:06:36 +0900

    feat: add jpaaudting

--- src/main/java/winterproject/market/MarketApplication.java
+++ src/main/java/winterproject/market/MarketApplication.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import winterproject.market.Service.MemberService;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MarketApplication {
 	public static void main(String[] args) {
